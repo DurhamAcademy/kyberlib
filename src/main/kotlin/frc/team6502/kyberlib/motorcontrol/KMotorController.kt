4ae2aab7786327de6d0253181cbd51f5b0fde187
20nelson <20nelson@da.org>
2019-07-12 20:48:13 -0400

    semi-working ksparkmax and test fixes

--- src/main/kotlin/frc/team6502/kyberlib/motorcontrol/KMotorController.kt
+++ src/main/kotlin/frc/team6502/kyberlib/motorcontrol/KMotorController.kt
@@ -2,24 +2,15 @@ package frc.team6502.kyberlib.motorcontrol
 
 import frc.team6502.kyberlib.math.units.Angle
 import frc.team6502.kyberlib.math.units.AngularVelocity
-
-fun talonsrx(canID: Int, init: KTalonSRX.() -> Unit): KTalonSRX {
-    val t = KTalonSRX(canID)
-    t.init()
-    return t
-}
-
+import frc.team6502.kyberlib.math.units.Length
 
 interface KMotorController {
     var percentOutput: Double
+    var kP: Double
+    var kI: Double
+    var kD: Double
+    var rotationSetpoint: Angle
+    var rotation: Angle
+    var positionSetpoint: Length
+    var position: Length
 }
-
-interface KClosedLoopMotorController {
-    var velocity: AngularVelocity
-    var sensorVelocity: AngularVelocity
-    var position: Angle
-    var sensorPosition: Angle
-}
-
-
-class ProfileParams(val maxVelocity: Double, val maxAcceleration: Double, val smoothing: Double = 0.0)
