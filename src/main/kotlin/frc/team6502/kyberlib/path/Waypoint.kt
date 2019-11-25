be32e12d97c23b0371fb02d27c0a17723dded44d
trevnels <20nelson@da.org>
2019-11-24 19:58:42 -0500

    removed in-house odometry and kinematics classes (thanks prateek)

--- src/main/kotlin/frc/team6502/kyberlib/path/Waypoint.kt
+++
@@ -1,8 +0,0 @@
-package frc.team6502.kyberlib.path
-
-import frc.team6502.kyberlib.math.geom.Vector2
-import frc.team6502.kyberlib.math.units.Angle
-
-data class Waypoint(val x: Double, val y: Double, val h: Angle){
-    val tangent get() = Vector2(h.cos, h.sin)
-}
