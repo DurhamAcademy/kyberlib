5751df6ff3524fcaf111455166f1f05a598a1c47
trevnels <20nelson@da.org>
2019-11-30 15:11:11 -0500

    allow for rgb wave to show under a full cycle within a region

--- src/main/kotlin/frc/team6502/kyberlib/lighting/animations/AnimationRGBWave.kt
+++ src/main/kotlin/frc/team6502/kyberlib/lighting/animations/AnimationRGBWave.kt
@@ -2,21 +2,27 @@ package frc.team6502.kyberlib.lighting.animations
 
 import java.awt.Color
 
-class AnimationRGBWave(private val cycles: Int = 1, val ticksPerMovement: Int, val reversed: Boolean = false) : LEDAnimation() {
+class AnimationRGBWave(private val cycles: Double = 1.0, val ticksPerMovement: Int = 10, val reversed: Boolean = false) : LEDAnimation() {
 
     fun constructInitialBuffer(length: Int): MutableList<Color> {
 
-        return Array<Color>(length) {
-            val hue = (it % (length / cycles)) / (length / cycles).toFloat()
-            Color.getHSBColor(hue, 1F, 1F)
-        }.asList().toMutableList()
-
+        return if (cycles >= 1) {
+            Array<Color>(length) {
+                val hue = (it % (length / cycles)) / (length / cycles)
+                Color.getHSBColor(hue.toFloat(), 1F, 1F)
+            }.asList().toMutableList()
+        } else {
+            Array<Color>((length / cycles).toInt()) {
+                val hue = it / (length / cycles)
+                Color.getHSBColor(hue.toFloat(), 1F, 1F)
+            }.asList().toMutableList()
+        }
     }
 
     override fun getBuffer(ticks: Int, length: Int): List<Color> {
         val b = constructInitialBuffer(length)
 
-        for (i in 0 until (ticks / ticksPerMovement) % length) {
+        for (i in 0 until (ticks / ticksPerMovement) % b.size) {
             b.add(0, b.removeAt(b.size - 1))
         }
 
