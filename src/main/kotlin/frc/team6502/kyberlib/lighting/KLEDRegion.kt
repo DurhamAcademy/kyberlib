6995ecd8f6778324c5a1634bf1cda6f7b916c2ab
trevnels <20nelson@da.org>
2019-11-30 17:50:27 -0500

    made LEDs testable

--- src/main/kotlin/frc/team6502/kyberlib/lighting/KLEDRegion.kt
+++ src/main/kotlin/frc/team6502/kyberlib/lighting/KLEDRegion.kt
@@ -5,6 +5,29 @@ import java.awt.Color
 
 class KLEDRegion(val animation: LEDAnimation, val start: Int, val end: Int, val enableTransparency: Boolean = false, val condition: () -> Boolean = { true }) {
 
+    companion object {
+        fun composite(length: Int, ticks: Int, regions: List<KLEDRegion>): Array<Color> {
+            val mutableBuffer = Array<Color>(length) { Color.BLACK }
+
+            for (region in regions) {
+                val b = region.getBuffer(ticks) ?: continue
+                for (i in b.indices) {
+                    if (b[i].alpha < 255 && region.enableTransparency) {
+                        mutableBuffer[region.start + i] = Color(
+                                (b[i].red / 255F) * (b[i].alpha / 255F) + (mutableBuffer[region.start + i].red / 255F) * (1 - b[i].alpha / 255F),
+                                (b[i].green / 255F) * (b[i].alpha / 255F) + (mutableBuffer[region.start + i].green / 255F) * (1 - b[i].alpha / 255F),
+                                (b[i].blue / 255F) * (b[i].alpha / 255F) + (mutableBuffer[region.start + i].blue / 255F) * (1 - b[i].alpha / 255F)
+                        )
+                    } else {
+                        mutableBuffer[region.start + i] = Color(b[i].red, b[i].green, b[i].blue)
+                    }
+                }
+            }
+
+            return mutableBuffer
+        }
+    }
+
     fun getBuffer(ticks: Int): List<Color>? {
         if (!condition()) return null
         val buf = animation.getBuffer(ticks, end - start).toMutableList()
--- src/main/kotlin/frc/team6502/kyberlib/lighting/KLEDStrip.kt
+++ src/main/kotlin/frc/team6502/kyberlib/lighting/KLEDStrip.kt
@@ -2,7 +2,6 @@ package frc.team6502.kyberlib.lighting
 
 import edu.wpi.first.wpilibj.AddressableLED
 import edu.wpi.first.wpilibj.AddressableLEDBuffer
-import java.awt.Color
 
 class KLEDStrip(port: Int, private val length: Int) {
 
@@ -30,23 +29,8 @@ class KLEDStrip(port: Int, private val length: Int) {
     }
 
     fun update() {
-        val mutableBuffer = Array<Color>(length) { Color.BLACK }
         val buffer = AddressableLEDBuffer(length)
-
-        for (region in regions) {
-            val b = region.getBuffer(ticks) ?: continue
-            for (i in b.indices) {
-                if (b[i].alpha < 255 && region.enableTransparency) {
-                    mutableBuffer[region.start + i] = Color(
-                            (b[i].red / 255F) * (b[i].alpha / 255F) + (mutableBuffer[region.start + i].red / 255F) * (1 - b[i].alpha / 255F),
-                            (b[i].green / 255F) * (b[i].alpha / 255F) + (mutableBuffer[region.start + i].green / 255F) * (1 - b[i].alpha / 255F),
-                            (b[i].blue / 255F) * (b[i].alpha / 255F) + (mutableBuffer[region.start + i].blue / 255F) * (1 - b[i].alpha / 255F)
-                    )
-                } else {
-                    mutableBuffer[region.start + i] = Color(b[i].red, b[i].green, b[i].blue)
-                }
-            }
-        }
+        val mutableBuffer = KLEDRegion.composite(length, ticks, regions)
 
         for (i in mutableBuffer.indices) {
             buffer.setLED(i, mutableBuffer[i].red, mutableBuffer[i].green, mutableBuffer[i].blue)
