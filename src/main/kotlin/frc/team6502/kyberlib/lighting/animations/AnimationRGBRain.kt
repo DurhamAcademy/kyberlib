9620806c4866edbecb6fe8e54c45046a2c26d5c1
trevnels <20nelson@da.org>
2020-02-25 21:14:23 -0500

    changed rain falloff

--- src/main/kotlin/frc/team6502/kyberlib/lighting/animations/AnimationRGBRain.kt
+++ src/main/kotlin/frc/team6502/kyberlib/lighting/animations/AnimationRGBRain.kt
@@ -2,6 +2,7 @@ package frc.team6502.kyberlib.lighting.animations
 
 import java.awt.Color
 import kotlin.math.ceil
+import kotlin.math.pow
 import kotlin.math.sqrt
 
 class AnimationRGBRain(private val cycles: Double = 1.0, private val dropLength: Int, val ticksPerMovement: Int, val reversed: Boolean = false) : LEDAnimation() {
@@ -9,7 +10,7 @@ class AnimationRGBRain(private val cycles: Double = 1.0, private val dropLength:
     fun constructInitialBuffer(length: Int): MutableList<Color> {
 
         return Array<Color>(dropLength * ceil(length.toDouble() / dropLength).toInt()) {
-            val alpha = ((1 + (it % dropLength)) / dropLength.toFloat())
+            val alpha = ((1 + (it % dropLength)) / dropLength.toFloat()).pow(2)
             Color(1F, 1F, 1F, alpha)
         }.toMutableList()
     }
--- src/main/kotlin/frc/team6502/kyberlib/lighting/animations/AnimationRain.kt
+++ src/main/kotlin/frc/team6502/kyberlib/lighting/animations/AnimationRain.kt
@@ -2,6 +2,7 @@ package frc.team6502.kyberlib.lighting.animations
 
 import java.awt.Color
 import kotlin.math.ceil
+import kotlin.math.pow
 import kotlin.math.sqrt
 
 class AnimationRain(private val color: Color, private val dropLength: Int, val ticksPerMovement: Int, val reversed: Boolean = false) : LEDAnimation() {
@@ -9,7 +10,7 @@ class AnimationRain(private val color: Color, private val dropLength: Int, val t
     fun constructInitialBuffer(length: Int): MutableList<Color> {
 
         return Array<Color>(dropLength * ceil(length.toDouble() / dropLength).toInt()) {
-            val alpha = ((1 + (it % dropLength)) / dropLength.toFloat())
+            val alpha = ((1 + (it % dropLength)) / dropLength.toFloat()).pow(2)
             Color(color.red / 255F, color.green / 255F, color.blue / 255F, alpha)
         }.toMutableList()
     }
